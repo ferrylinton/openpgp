ec4c6e88158eeabf41cdeba3017ff6eed4de4f25
Ferry Linton Hutapea <ferrylinton@gmail.com>
2017-07-18 17:30:31 +0700

    update

--- src/main/resources/static/app/home/home.controller.js
+++ src/main/resources/static/app/home/home.controller.js
@@ -10,6 +10,40 @@
 
 		var vm = this;
 		
+		vm.encrypt = { 
+				data : 'test 1234',
+				passwords : ['password'],
+				armor : true,
+				result : ''
+		};
+		
+		vm.decrypt = { 
+				message : '',
+				password : vm.encrypt.passwords[0],
+				result : ''
+		};
+		
+		vm.submitEncrypt = submitEncrypt;
+		vm.submitDecrypt = submitDecrypt;
+		
+		
+		function submitEncrypt(){
+			openpgp.encrypt(vm.encrypt).then(function(ciphertext) {
+				vm.encrypt.result = ciphertext.data;
+				vm.decrypt.message = ciphertext.data;
+				$scope.$apply();
+			});
+		}
+		
+		function submitDecrypt(){
+			var options = JSON.parse(JSON.stringify(vm.decrypt));
+			options.message = openpgp.message.readArmored(options.message);
+			
+			openpgp.decrypt(options).then(function(plaintext) {
+				vm.decrypt.result = plaintext.data;
+				$scope.$apply();
+			});
+		}
 		
 	}
 
